69ec7246660c6d21eb60a50e13c0fe7278b7d40f
pulkit185 <pulkit185@gmail.com>
2020-07-11 00:55:16 +0530

    Initial commit

---
+++ src/main/java/com/bansalfurnitures/homedecor/HomedecorApplication.java
@@ -0,0 +1,13 @@
+package com.bansalfurnitures.homedecor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomedecorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomedecorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bansalfurnitures/homedecor/HomedecorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bansalfurnitures.homedecor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomedecorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
